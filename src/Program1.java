20b084c4559b087ab3d57cdb0e7de4c7552b56d6
SwapnaliJ <swapnalij4050@gmail.com>
2020-12-03 14:44:54 +0530

    firstvpmmit

---
+++ src/Program1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Program1 {
+public static void main(String[] args) {
+				// TODO Auto-generated method stub
+				Scanner in= new Scanner(System.in);
+				int j=in.nextInt();
+				
+			if(j>5)
+			
+			{
+				System.out.println("Hello");
+			}
+			else System.out.println("Bye");
+
+			
+			}
+
+		
+	}
+
+
